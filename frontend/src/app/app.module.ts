73a8c8f599f7a13c166de442f21a65f47d64f783
Aleksey Zhukov <alex__zhukov@mail.ru>
2024-06-23 11:09:57 +0300

    before creating snackBar component

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './shared/layout/layout.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { MainComponent } from './views/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/views/user/login/login.component.ts
@@ -0,0 +1,80 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {AuthService} from "../../../core/auth/auth.service";
+import {DefaultResponseType} from "../../../../types/default-response.type";
+import {LoginResponseType} from "../../../../types/login-response.type";
+import {HttpErrorResponse} from "@angular/common/http";
+import {
+  MatSnackBar, MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from "@angular/material/snack-bar";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]],
+    rememberMe: [false],
+  });
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+
+  constructor(private fb: FormBuilder, private authService: AuthService, private _snackBar: MatSnackBar, private router: Router) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  login(): void {
+    if (this.loginForm.valid && this.loginForm.value.email && this.loginForm.value.password) {
+      this.authService.login(this.loginForm.value.email, this.loginForm.value.password, !!this.loginForm.value.rememberMe)
+        .subscribe({
+          next: (data: DefaultResponseType | LoginResponseType) => {
+            let error = null;
+            if ((data as DefaultResponseType).error !== undefined) {
+              error = (data as DefaultResponseType).message;
+            }
+            const loginResponse = data as LoginResponseType;
+            if (!loginResponse.accessToken || !loginResponse.refreshToken || !loginResponse.userId) {
+              error = 'Ошибка авторизации';
+            }
+            if (error) {
+              this._snackBar.open(error, '', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition
+              })
+              throw new Error(error)
+            }
+
+            //set tokens
+
+            this._snackBar.open('Вы успешно авторизовались', '', {
+              horizontalPosition: this.horizontalPosition,
+              verticalPosition: this.verticalPosition
+            })
+            this.router.navigate(['/']);
+          },
+          error: (errorResponse: HttpErrorResponse) => {
+            if (errorResponse.error && errorResponse.error.message) {
+              this._snackBar.open(errorResponse.error.message, '', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition
+              })
+            } else {
+              this._snackBar.open('Ошибка авторизации', '', {
+                horizontalPosition: this.horizontalPosition,
+                verticalPosition: this.verticalPosition
+              })
+            }
+          }
+        })
+    }
+  }
+
+}
